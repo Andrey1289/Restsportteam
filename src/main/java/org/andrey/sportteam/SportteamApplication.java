497aa0ff5bf7a0c871aa27730b394dc79249a091
Andrey Galkovskii <dobpuha@gmail.com>
2023-03-20 13:29:42 +0700

    start empty sport team project

---
+++ src/main/java/org/andrey/sportteam/SportteamApplication.java
@@ -0,0 +1,13 @@
+package org.andrey.sportteam;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportteamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportteamApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/andrey/sportteam/SportteamApplicationTests.java
@@ -0,0 +1,13 @@
+package org.andrey.sportteam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SportteamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
